873aa7e1ab0d8088dc63376f871270c9a0a60b7e
David Driscoll <david.driscoll@gmail.com>
2019-06-20 20:22:43 -0400

    Use instant instead of datetimeoffset +semver:major

--- src/Domain.Abstractions/ChangeData.cs
+++ src/Domain.Abstractions/ChangeData.cs
@@ -1,21 +1,30 @@
 ﻿using Newtonsoft.Json;
+using NodaTime;
 using System;
 
 namespace Rocket.Surgery.Domain
 {
     public class ChangeData<TKey>
     {
-
         [JsonConstructor]
-        public ChangeData(TKey by, DateTimeOffset? at)
+        public ChangeData(TKey by, Instant? at)
         {
             By = by;
             At = at;
         }
 
+        public ChangeData(TKey by, DateTimeOffset? at)
+        {
+            By = by;
+            if (at.HasValue)
+            {
+                At = Instant.FromDateTimeOffset(at.Value);
+            }
+        }
+
         public ChangeData() { }
 
         public TKey By { get; }
-        public DateTimeOffset? At { get; }
+        public Instant? At { get; }
     }
 }
